e54a21bd2c37b4c6a236481193c32251829b0fe5
Ashish Pahwa <ashishpahwa7@gmail.com>
2016-06-07 22:58:17 +0530

    updated - Support Vector Machine

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -20,11 +20,44 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+##features_train = features_train[:len(features_train)/100] 
+##labels_train = labels_train[:len(labels_train)/100] 
 
 
 #########################################################
 ### your code goes here ###
 
-#########################################################
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
+clf = SVC(kernel = "rbf",C= 10000.0)
+t0 = time()
+
+clf.fit(features_train,labels_train)
+
+print "training time :" , round(time()-t0,3),"s"
+
+t1 = time()
+
+predict = clf.predict(features_test)
+
+
+print "predicting time :" , round(time()-t1,3),"s"
+
+
+print "Accuracy Rate : %f" % accuracy_score(labels_test, predict)
+
+ans1 = predict[10]
+ans2 = predict[26]
+ans3 = predict[50]
+
+print sum(predict)
+
+print ans1
+print ans2
+print ans3
+
+
+
+#########################################################
 
